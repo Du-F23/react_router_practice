201d06b20e18e7f7b57b813cf4a702dc7afedd11
2021 DSM53 Duarte V Fernando <al222010329@gmail.com>
2022-01-27 14:33:26 -0600

    Constructor

--- src/components/Contenido2.js
+++ src/components/Contenido2.js
@@ -1,27 +1,39 @@
 import React from "react";
-import img1 from "../img/images.png";
+import img from "../img/images.png";
 
 export default class Contenido2 extends React.Component{
 
 constructor(props){
     super(props);
     this.state = {
-        img: img1,
+        img: img,
         pdf: 'https://code.visualstudio.com/shortcuts/keyboard-shortcuts-windows.pdf',
         texto:'Texto mandado a traer desde el constructor',
         datos: 'Datos mandados a traer desde el constructor',
     }
 }
 
+componentDidMount(){
+    setTimeout(() => {
+this.setState({
+    img: this.props.img,
+    pdf: this.props.pdf,
+    texto: this.props.texto,
+    datos: this.props.datos,
+},5000)
+    })
+}
+
     render(){
+        const {img, pdf, texto, datos} = this.state
         return(
             <div>
                 <h3>Contenido 2</h3>
-                <img src={img} alt=""/>
+                <img src={img} alt="Imagen Props"/>
                 <a href={pdf}><button className="btn btn-outline-success">UTVT</button></a>
-
                 <hr/>
-                {texto} {datos}
+                {texto}
+                {datos}
             </div>
         )
     }
